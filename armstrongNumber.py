7bb5746e882627b6c7c2ff23c3751847d5adec7b
Toxic <120262706+ToxicCharged@users.noreply.github.com>
2022-12-13 20:27:32 +0530

    Add files via upload

---
+++ armstrongNumber.py
@@ -0,0 +1,18 @@
+def main():
+
+    print()
+    num = int(input("Enter a number: "))
+    temp = num
+    sum = 0
+    digits = len(str(num))
+
+    temp = num
+
+    while temp > 0:
+        sum += (temp % 10) ** digits
+        temp //= 10
+
+    if sum == num:
+        print("It is an armstrong number.")
+    else:
+        print("It is not an armstrong number.")
---
+++ reverseOfNumber.py
@@ -0,0 +1,17 @@
+def main():
+
+    print()
+    num = int(input("Enter a number: "))
+    temp = abs(num)
+    rev_num = 0
+    placeValue = len(str(num)) - 1
+
+    while temp > 0:
+        rev_num += (temp % 10) * 10 ** placeValue
+        temp //= 10
+        placeValue -= 1
+
+    if num < 0:
+        rev_num *= -1
+
+    print("It's reverse =", rev_num)
